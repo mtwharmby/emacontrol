9f02e0b18c21c9bb23fdb0ea4420eda50b2f2680
Michael Wharmby <mtwharmby@gmail.com>
2017-10-27 00:33:15 +0200

    Adds tests of mount_sample and unmount_sample series of states.
    Signed-off-by: Michael Wharmby <mtwharmby@gmail.com>

--- ema_control/core.py
+++ ema_control/core.py
@@ -7,9 +7,19 @@ class Robot:
         self.do_state_change()
 
     def do_state_change(self):
-        self.state=None
+        self.status=None
         raise NotImplementedError("No state change operation defined")
+
+    def mount_sample(self):
+        raise NotImplementedError("Cannot 'mount_sample' from status '%s'" % self.status)
+
+    def unmount_sample(self):
+        raise NotImplementedError("Cannot 'unmount_sample' from status '%s'" % self.status)
+
 
 class RobotReady(Robot):
     def do_state_change(self):
-        self.state = "READY"
+        self.status = "Ready"
+
+class RobotParked(Robot):
+    pass
--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -1,4 +1,4 @@
-from ema_control.core import Robot
+from ema_control.core import Robot, RobotParked
 
 from nose.tools import assert_equal
 
@@ -9,4 +9,40 @@ class TestStateMachine:
 
     def test_initialise(self):
         '''Robot should start in the READY state'''
-        assert_equal(self.robot.state, "READY")
+        assert_equal(self.robot.status, "Ready")
+
+    def test_mount_sample_transitions(self):
+        '''Mount sample should have the following series of transitions:
+        Ready -> Picking sample -> Mounting sample -> Parking -> Parked
+        Another call to mount_sample should throw an error'''
+
+        self.robot.mount_sample() #User trigger
+        assert_equal(self.robot.status, "Picking sample")
+        self.robot.mount_sample() #H/W trigger
+        assert_equal(self.robot.status, "Mounting sample")
+        #Has sample = true
+        self.robot.mount_sample() #H/W trigger
+        assert_equal(self.robot.status, "Parking")
+        #has_sample = false
+        #sample_mounted = true
+        self.robot.mount_sample() #H/W trigger
+        assert_equal(self.robot.status, "Parked")
+
+    def test_unmount_sample_transitions(self):
+        '''Unmount sample should have the following transitions:
+        Parked -> Recovering sample -> Unmounting sample -> Parking -> Ready
+        Another call to unmount_sample should throw an error'''
+
+        #Set the initial state to Parked
+        self.robot.next_state(RobotParked)
+
+        self.robot.unmount_sample() #User trigger
+        assert_equal(self.robot.status, "Recovering sample")
+        self.robot.unmount_sample() #H/W trigger
+        assert_equal(self.robot.status, "Unmounting sample")
+        self.robot.unmount_sample() #H/W trigger
+        assert_equal(self.robot.status, "Parking")
+        self.robot.unmount_sample() #H/W trigger
+        assert_equal(self.robot.status, "Ready")
+
+        #Need to test location of sample before making some (all?) transitions
