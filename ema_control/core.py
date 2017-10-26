2a2881be2202f32a66dfcd72b4edcd6fb13fd15b
Michael Wharmby <mtwharmby@gmail.com>
2017-10-26 23:59:04 +0200

    Implements the initialisation of the robot to the READY state.
    Signed-off-by: Michael Wharmby <mtwharmby@gmail.com>

--- ema_control/core.py
+++ ema_control/core.py
@@ -1,2 +1,15 @@
 class Robot:
-    pass
+    def __init__(self):
+        self.next_state(RobotReady)
+
+    def next_state(self, state):
+        self.__class__ = state
+        self.do_state_change()
+
+    def do_state_change(self):
+        self.state=None
+        raise NotImplementedError("No state change operation defined")
+
+class RobotReady(Robot):
+    def do_state_change(self):
+        self.state = "READY"
