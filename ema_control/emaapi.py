9989d82b0a9f853e4db353b6580aae55e4801ef8
Michael Wharmby <mtwharmby@gmail.com>
2017-10-26 22:55:39 +0200

    Refactor ema-api module name to more general ema_control. Style to agree with PEP-8.
    Signed-off-by: Michael Wharmby <mtwharmby@gmail.com>

---
+++ ema_control/emaapi.py
@@ -0,0 +1,28 @@
+class EmaApi:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def mount_sample(self, n):
+        return self.mount_samples([n])
+
+    def mount_samples(self, mn):
+        pass
+
+    def unmount_sample(self):
+        pass
+
+    def clear_sample(self):
+        pass
+
+    def make_safe(self):
+        pass
+
