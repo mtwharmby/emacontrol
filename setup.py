17422e21ef08864e6f41ccdf15e72dabcf79eb2f
Michael Wharmby <michael.wharmby@desy.de>
2017-11-09 19:32:47 +0100

    Corrects setup.py package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='ema-control',
+setup(name='emacontrol',
       version='0.1',
       description='User interface and control server for the P02.1 sample robot - "EMA"',
       author='Mario Wendt, Michael Wharmby',
       author_email='mario.wendt@desy.de',
-      packages=['ema_control'],
+      packages=['emacontrol'],
       )
