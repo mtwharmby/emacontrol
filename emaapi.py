102ad148b5a3a4f0642c1ceec9aeb9ae32a4e530
Michael Wharmby <mtwharmby@gmail.com>
2019-06-10 19:20:52 +0200

    Adds all simple methods to API
    Simple methods are those that just send a message to the robot and do
    not wait for anything to return

--- emaapi.py
+++ emaapi.py
@@ -1,12 +1,12 @@
-'''
+"""
 Robot class should create a socket and provide methods to use that socket to pass instructions through that socket to the robot.
 Methods on the module should then use an instance of this class to send commands to achieve tasks
 
 methods required:
-- power_on
-- power_off
-- start
-- stop
+X power_on
+X power_off
+X start
+X stop
 - set_axis
 - homing
 - power_status
@@ -15,7 +15,7 @@ methods required:
 - (status)
 - (calibrate_sam)
 - (compare_sam)
-'''
+"""
 
 
 class Robot():
@@ -24,8 +24,38 @@ class Robot():
         pass
 
 
+def power_off():
+    """
+    Disables power to the robot.
+    """
+    ema.send('powerOff')
+
+
 def power_on():
+    """
+    Enables power to the robot.
+    """
     ema.send('powerOn')
+
+
+def reset():
+    # TODO: Needs doc
+    ema.send('reset')
+
+
+def restart():
+    # TODO: Needs doc
+    ema.send('restartMotor')
+
+
+def start():
+    # TODO: Needs doc
+    ema.send('start')
+
+
+def stop():
+    # TODO: Needs doc
+    ema.send('stopMotor')
 
 
 ema = Robot()
--- test_emaapi.py
+++ test_emaapi.py
@@ -1,4 +1,4 @@
-'''
+"""
 - power_on
 - power_off
 - start
@@ -11,14 +11,43 @@
 - (status)
 - (calibrate_sam)
 - (compare_sam)
-'''
+"""
 from mock import patch
 
-from emaapi import (power_on)
+from emaapi import (power_off, power_on, reset, restart, start, stop)
+
+
+@patch('emaapi.ema.send')
+def test_power_off(ema_send_mock):
+    power_off()
+    ema_send_mock.assert_called_with('powerOff')
 
 
 @patch('emaapi.ema.send')
 def test_power_on(ema_send_mock):
     power_on()
-
     ema_send_mock.assert_called_with('powerOn')
+
+
+@patch('emaapi.ema.send')
+def test_reset(ema_send_mock):
+    reset()
+    ema_send_mock.assert_called_with('reset')
+
+
+@patch('emaapi.ema.send')
+def test_restart(ema_send_mock):
+    restart()
+    ema_send_mock.assert_called_with('restartMotor')
+
+
+@patch('emaapi.ema.send')
+def test_start(ema_send_mock):
+    start()
+    ema_send_mock.assert_called_with('start')
+
+
+@patch('emaapi.ema.send')
+def test_stop(ema_send_mock):
+    stop()
+    ema_send_mock.assert_called_with('stopMotor')
