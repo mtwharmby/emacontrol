3b8866ffcf97982543bedfe90414171dd125050b
Michael Wharmby <mtwharmby@gmail.com>
2019-06-19 01:25:07 +0200

    Adds docstrings to database access methods
    Also adds stub for get_session_id_for_today

--- emadb.py
+++ emadb.py
@@ -5,7 +5,27 @@ def __sql_query__(conn, sql):
     pass
 
 
+def get_session_id_for_today(conn):
+    pass
+
+
 def get_appids_for_session(conn, session_id=None):
+    """
+    Get all application IDs associated with a specific session in the database
+
+    Parameters
+    ----------
+    conn : a connection to an SQLite database
+    session_id : (optional) integer representing a given session. If not given,
+                 session_id is determined using get_session_id_for_today()
+
+    Returns
+    -------
+    list : integers representing the application IDs
+    """
+    if session_id is None:
+        session_id = get_session_id_for_today(conn)
+
     app_ids = []
     result = __sql_query__(conn, ("""SELECT application_id FROM Applications
                                      WHERE session_id=?""", (session_id,)))
@@ -15,6 +35,25 @@ def get_appids_for_session(conn, session_id=None):
 
 
 def get_samples_for_appid(conn, measurement, app_id):
+    """
+    For a given application ID, get all of the samples which are to be measured
+    with a given measurment type
+
+    Parameters
+    ----------
+    conn : a connection to an SQLite database
+    measurement : string, either PXRD or PDF
+    app_id : integer representing application ID
+
+    Returns
+    -------
+    dict : keys are integer positions of samples in magazine,
+           values are names of samples
+
+    Raises
+    ------
+    KeyError : if measurement type other than 'PXRD' or 'PDF' is requested
+    """
     measurement_sql = {'PXRD': """SELECT holder_position,user_sample_name FROM
                                   PXRD_Samples WHERE application_id=?""",
                        'PDF': """SELECT holder_position,user_sample_name FROM
