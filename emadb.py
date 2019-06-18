d6ed9fbb6eebb12880db44c1070e16c0334abf15
Michael Wharmby <mtwharmby@gmail.com>
2019-06-19 01:10:40 +0200

    Adds samples for appid method and test

--- emadb.py
+++ emadb.py
@@ -7,9 +7,22 @@ def __sql_query__(conn, sql):
 
 def get_appids_for_session(conn, session_id=None):
     app_ids = []
-    result = __sql_query__(conn, 
-                           ('SELECT application_id FROM Applications WHERE\
-                               session_id=?', (session_id,)))
+    result = __sql_query__(conn, ("""SELECT application_id FROM Applications
+                                     WHERE session_id=?""", (session_id,)))
     for row in result:
         app_ids.append(row[0])
     return app_ids
+
+
+def get_samples_for_appid(conn, measurement, app_id):
+    measurement_sql = {'PXRD': """SELECT holder_position,user_sample_name FROM
+                                  PXRD_Samples WHERE application_id=?""",
+                       'PDF': """SELECT holder_position,user_sample_name FROM
+                                  PDF_Samples WHERE application_id=?""",
+                       }
+
+    samples = {}
+    result = __sql_query__(conn, (measurement_sql[measurement], (app_id,)))
+    for row in result:
+        samples[row[0]] = row[1]
+    return samples
--- test_emadb.py
+++ test_emadb.py
@@ -4,9 +4,10 @@ Output needs to be a dictionary of application_IDs, each with a dictionary of sa
 To do this, get list of applications from DB - based on session (this determined from date?)
 Then get list of all samples in table with that app_ID - which table?
 """
+import pytest
 from mock import patch, Mock
 
-from emadb import get_appids_for_session
+from emadb import get_appids_for_session, get_samples_for_appid
 
 
 def test_get_appids_for_session():
@@ -14,9 +15,36 @@ def test_get_appids_for_session():
 
     with patch('emadb.__sql_query__') as sql_mock:
         sql_mock.return_value = [(120,), (522,), (742,)]
-        app_ids = get_appids_for_session(dbconn_mock, session_id=1)
 
-        sql_mock.assert_called_with(dbconn_mock,
-                                    ('SELECT application_id FROM Applications WHERE\
-                               session_id=?', (1,)))
+        app_ids = get_appids_for_session(dbconn_mock, session_id=1)
+        sql_mock.assert_called_with(dbconn_mock, ("""SELECT application_id FROM Applications
+                                     WHERE session_id=?""", (1,)))
         assert app_ids == [120, 522, 742]
+
+
+def test_get_samples_for_appid():
+    dbconn_mock = Mock()
+
+    with patch('emadb.__sql_query__') as sql_mock:
+        sql_mock.return_value = [(1, 'sample A',), (2, 'sample Q')]
+
+        # Check getting PXRD samples...
+        samples = get_samples_for_appid(dbconn_mock, 'PXRD', 120)
+        sql_mock.assert_called_with(dbconn_mock, (
+            """SELECT holder_position,user_sample_name FROM
+                                  PXRD_Samples WHERE application_id=?""",
+                                   (120,)))
+        assert samples == {1: 'sample A', 2: 'sample Q'}
+
+        # Check getting PDF samples...
+        samples = get_samples_for_appid(dbconn_mock, 'PDF', 120)
+        sql_mock.assert_called_with(dbconn_mock, (
+            """SELECT holder_position,user_sample_name FROM
+                                  PDF_Samples WHERE application_id=?""",
+                                   (120,)))
+
+        # Check getting some non-existant measurement type...
+        # ... this should throw an error
+        with pytest.raises(KeyError, match=r"LightScattering"):
+            samples = get_samples_for_appid(dbconn_mock, 'LightScattering', 
+                                            120)
