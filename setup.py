592e09ade35ef5593dcb65f0271398769aaf902d
Michael Wharmby <mtwharmby@gmail.com>
2019-07-31 10:26:32 +0200

    Adds missing pacakge gevent to setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ VERSION = '1.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    # 'numpy', 'requests', 'uncertainties',
+    'gevent',
 ]
 
 # What packages are optional?
