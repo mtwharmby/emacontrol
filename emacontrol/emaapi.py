fdb3f6ddd2a552535aa22fce581f305b2919b07d
Michael Wharmby <mtwharmby@gmail.com>
2020-10-16 11:37:05 +0200

    Changes start behaviour
    Checks sample number rather than coordinate.

--- emacontrol/emaapi.py
+++ emacontrol/emaapi.py
@@ -27,15 +27,14 @@ def robot_begin():
     # TODO Ideally this would check the interlock programmatically. But this
     # isn't an option yet.
     input('Have you pressed the reset button?\nPress enter to continue...')
-    gotSamPos = ema.send('getSamPosOffset;')
-    sam_pos = gotSamPos.state
-    if sam_pos != {'X': 0, 'Y': 0}:  # FIXME Check flag; check powerstate
-        sample = (sam_pos['X'] * 10) + sam_pos['Y'] + 1
-        ema.sample_index = sample
+    sam_pos = ema.get_sample_number()
+
+    if sam_pos != 1:  # FIXME Check flag; check powerstate
+        ema.sample_index = sam_pos  # FIXME This shoudn't be needed...?
         # TODO Log: 'Sample coords at robot start are ({}, {}) (Sample {}).
         # Should be (0, 0) for Sample 1'.format(coords['X'], coords['Y'],
         # sample)
-        print('WARNING: Current sample is {} (not 1!).'.format(sample))
+        print('WARNING: Current sample is {} (not 1!).'.format(sam_pos))
         print('Is there a sample on the spinner? '
               + 'Run \'unmount_sample()\' immediately if there is!')
 
